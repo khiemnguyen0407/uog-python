fe5e672bf93f3700561451c879c849b40490751e
cafesua <herokhiem@yahoo.com>
2025-10-05 16:46:54 +0100

    remove folder fem/

--- fem/grid/__init__.py
+++
@@ -1,1 +0,0 @@
-print("init: fem/grid")
--- fem/grid/triangulation.py
+++
@@ -1,8 +0,0 @@
-class Triangulation:
-    def __init__(self):
-        print("Constructor")
-        pass
-
-    def make_grid(self):
-        print("Make grid")
-        pass
