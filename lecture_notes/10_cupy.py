fb2dc78f31dd3f870d62021b1e891661d7fa8fa2
Khiem Nguyen <ln69g@login1.cognition.gla.alces.network>
2025-12-09 15:20:56 +0000

    Update multiple files * 01_variables_and_operators.ipynb * 03_builtin_data_structures.ipynb * 10_cupy.py

--- lecture_notes/10_cupy.py
+++ lecture_notes/10_cupy.py
@@ -3,22 +3,5 @@ import numpy as np
 import cupy as cp
 import time
 
-print(f"Cuda is avaiable: {cp.is_available()}")
+print(f"cupy is avaiable: {cp.is_available()}")
 # %%
-tstart = time.time()
-A_cpu = np.linspace(0, 1, 10000000).reshape(10000, 1000)
-b_cpu = np.linspace(1, 2, 1000)
-for iter in range(500):
-    dot_prod = np.dot(A_cpu, b_cpu)
-
-tend = time.time()
-print(f"telapsed (CPU) = {tend - tstart}")
-
-# %%
-tstart = time.time()
-A_gpu = cp.linspace(0, 1, 10000000).reshape(10000, 1000)
-b_gpu = cp.linspace(1, 2, 1000)
-for iter in range(500):
-    dot_prod = cp.dot(A_gpu, b_gpu)
-tend = time.time()
-print(f"telapsed (GPU) = {tend - tstart}")
